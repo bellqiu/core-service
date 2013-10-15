7cf7a09ad7fe50f08f5d479dd151faa39a8b98cc
qiulhong@gmail.com <qiulhong@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2013-10-15 17:10:02 +0000

    Add html service wrapper
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/core-service@2015 c8aa77e7-4d14-485d-9416-cb5e655923dd

---
+++ src/main/java/com/honeybuy/shop/web/cache/HtmlServiceCacheWraper.java
@@ -0,0 +1,20 @@
+package com.honeybuy.shop.web.cache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.googlecode.ehcache.annotations.Cacheable;
+import com.hb.core.entity.HTML;
+import com.hb.core.service.HtmlService;
+
+@Service
+public class HtmlServiceCacheWraper {
+	
+	@Autowired
+	private HtmlService htmlService;
+	
+	@Cacheable(cacheName="HtmlServiceCacheWraper_getStringValue")
+	public HTML getHTML(String key){
+		return htmlService.getHTML(key);
+	}
+}
