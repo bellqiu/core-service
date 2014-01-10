dade73a5f3191d88113579d469d99235795783b1
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2014-01-10 02:06:41 +0000

    Paypal notify
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/core-service@2306 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- src/main/java/com/honeybuy/shop/web/ShoppingController.java
+++ src/main/java/com/honeybuy/shop/web/ShoppingController.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -400,7 +399,7 @@ public class ShoppingController {
 			
 			if("paypal".equals(payment)){
 				
-				String serverHost = "http://"+request.getServerName() ;
+				String serverHost = siteService.getSite().getDomain() ;
 				
 				model.addAttribute("notifyUrl", serverHost + settingService.getStringValue(Constants.PAYPAL_NOTIFY_URI, Constants.PAYPAL_NOTIFY_URI_DEFAULT));
 				
